9ca38008a816aa722cb1762d4be9860e2c4e2024
adriankr38 <adrian58ago@gmail.com>
2023-02-26 20:51:17 +0000

    Add(Jsx): `Routes`

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import Analytics from "./components/analytics.js";
 import Versus from "./components/versus.js";
 import Menu from "./components/menu.js";
 import { ThemeProvider } from "./context/theme.js";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
 import "../dist/output.css";
 
 class App extends React.Component {
@@ -24,20 +26,27 @@ class App extends React.Component {
   }
   render() {
     const lightBg =
-      "min-w-screen h-full px-5 py-3 bg-gradient-to-br bg-fixed from-sky-100 to-purple-100 sm:bg-gradient-to-br sm:from-purple-50 sm:via-sky-100 sm:to-purple-100";
+      "min-w-screen h-screen px-5 py-3 clear-both bg-gradient-to-br bg-cover from-sky-100 to-purple-100 sm:bg-gradient-to-br sm:from-purple-50 sm:via-sky-100 sm:to-purple-100";
     const darkBg =
-      "min-w-screen h-full px-5 py-3 bg-gradient-to-tr from-[black_80%] via-slate-800 to-rose-800 bg-fixed ";
+      "min-w-screen h-screen clear-both px-5 py-3 bg-gradient-to-tr from-[rgba(0,0,0,0.95)_80%] via-slate-800 to-rose-800 bg-cover";
     const { theme } = this.state;
     return (
       <ThemeProvider value={this.state}>
         <div className={theme === "light" ? lightBg : darkBg}>
-          <div className="min-w-screen h-full">
-            {/* <Analytics /> */}
-            {/* <Trending />*/}
-            {/*<Home />*/}
-            <Home />
-            {/* <Versus /> */}
-          </div>
+          {/*<div className="min-w-screen h-full">*/}
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/analytics" element={<Analytics />} />
+              <Route path="/trending" element={<Trending />} />
+              <Route path="/versus" element={<Versus />} />
+              {/* <Analytics /> */}
+              {/* <Trending />*/}
+              {/*<Home />*/}
+              {/* <Versus /> */}
+            </Routes>
+          </BrowserRouter>
+          {/*</div>*/}
         </div>
       </ThemeProvider>
     );
